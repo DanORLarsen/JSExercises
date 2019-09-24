f96fbe50d1545ca7687ddd8ab1b71412321d4254
DanORLarsen <42993978+DanORLarsen@users.noreply.github.com>
2019-09-24 18:01:16 +0200

    Cleanup and perfecting the code

--- every.js
+++ every.js
@@ -1,19 +1,12 @@
 function every(arrayTest, test) {
-    let trueOrFalse;
+    let trueOrFalse = true;
     arrayTest.forEach(element => {
         if(!test(element)){
             trueOrFalse = false;
         }
     });
-    //Would always break loop if false and then return true, if i just said return false in forEach.
-    //Because it would still execute the rest, ( return true; after it was ordered to return false;)
-    //Which indicates that return only breaks the current bracket... 
-    if(typeof trueOrFalse === 'undefined')
-    {return true;}
     return trueOrFalse;
   } 
-  let d = [1,2,3]
-  console.log(d[0]);
   
   console.log(every([1, 3, 5], n => n < 10));
   // → true
