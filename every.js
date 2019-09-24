ce12a71749440c4897184f5dda4a357978051b40
DanORLarsen <42993978+DanORLarsen@users.noreply.github.com>
2019-09-24 17:58:23 +0200

    return only breaks one bracket???

--- every.js
+++ every.js
@@ -1,6 +1,18 @@
-function every(array, test) {
-    // Your code here.
+function every(arrayTest, test) {
+    let trueOrFalse;
+    arrayTest.forEach(element => {
+        if(!test(element)){
+            trueOrFalse = false;
+        }
+    });
+    //Would always break loop if false and then return true, if i just said return false in forEach.
+    //Because it would still execute the rest, ( return true; after it was ordered to return false;)
+    if(typeof trueOrFalse === 'undefined')
+    {return true;}
+    return trueOrFalse;
   }
+  let d = [1,2,3]
+  console.log(d[0]);
   
   console.log(every([1, 3, 5], n => n < 10));
   // → true
