1e2c440754b046e27be6051e7c0860be62c8ae1c
DanORLarsen <42993978+DanORLarsen@users.noreply.github.com>
2019-09-25 15:53:03 +0200

    Doing objectExercises

---
+++ objectExercise1.js
@@ -0,0 +1,17 @@
+//Fix syntax etc.
+
+let person = {firstName: "Josh", lastname: "Lehman" }
+console.log(person);
+
+
+let numbers = {a: 1, b: 2, c: 3, d: 4}
+console.log(numbers)
+let animals = {
+    animal: "dog" ,
+    noise: "bark",
+        age: 3,
+         type: "Labrador",
+     color: "Yellow"
+}
+console.log(animals);
+
